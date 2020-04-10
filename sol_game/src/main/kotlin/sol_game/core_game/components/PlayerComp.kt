d26898b94e2a63ad5f8d2fcedb5386c044a10c2e
Eirik Skjærseth <eirik1502@gmail.com>
2020-04-10 15:26:14 +0200

    clean up old unused files

--- sol_game/src/main/kotlin/sol_game/core_game/components/PlayerComp.kt
+++
@@ -1,5 +0,0 @@
-package sol_game.core_game.components
-
-import sol_engine.ecs.Component
-
-class PlayerComp() : Component()
--- sol_game/src/main/kotlin/sol_game/core_game/components/SolStatePacketComp.kt
+++
@@ -1,8 +0,0 @@
-package sol_game.core_game.components
-
-import sol_engine.ecs.Component
-import sol_game.core_game.SolGameStatePacket
-
-data class SolStatePacketComp(
-        var statePacket: SolGameStatePacket = SolGameStatePacket()
-) : Component()
